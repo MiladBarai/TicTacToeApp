82e68bd63ded3995bed48fc41ce3369128045737
Milad Barai <MiladBarai@gmail.com>
2020-01-16 04:14:13 +0100

    finalized layout with buttons having onClick events

--- app/src/main/java/com/onlyi/and/ai/tictactoe/MainActivity.kt
+++ app/src/main/java/com/onlyi/and/ai/tictactoe/MainActivity.kt
@@ -2,11 +2,23 @@ package com.onlyi.and.ai.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun signPlaced( view:View){
+
+        //Convert view to button
+        val placedOnButton = view as Button
+
+        Log.d("button", "sign " + "X" + " placed on: "+placedOnButton.id.toString())
     }
 }
